c1fb95828ebf48882adae00c5d82447222de1160
Omar Iqbal <oiqbal@hydra15.eecs.utk.edu>
2019-03-29 13:13:28 -0400

    added CentralCircle class

---
+++ src/CentralCircle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Circle{
+
+	/*angular position of point
+	 * along circumference (0deg - 360deg ccw)*/
+	int angularPointPos;
+
+	int radius;
+
+};
+
+//testing main
+int main(){return 0;}
